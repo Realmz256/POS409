3148fdc9b09f18948c54d5fe3126f7d7176569e3
Kayla Nailor <knailor19@email.phoenix.edu>
2018-05-21 20:32:44 -0400

    Updated Navigator class, removed avoid/include items

---
+++ Directions/Directions/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directions
+{
+    class Navigator
+    {
+        string startPoint;
+        string endPoint;
+        //is the API key a field in the class?
+
+        //Methods
+
+        SetCurrentLocation()
+            //Read text from From field
+
+        SetDestination()
+            //Read text from To field
+        
+        GetDirections()
+            //I think this involves the API to determine the directions?
+        
+        GetTimeToDestination()
+            //We don't have to include this
+        
+        TotalDistance()
+            //We don't have to include this, either.
+    }
+}
